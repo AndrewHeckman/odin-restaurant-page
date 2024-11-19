c6700a7faae3adf17227a40907e7282a4fd2debe
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-11-19 16:32:37 -0600

    Add tab switching event listener to nav

--- src/index.js
+++ src/index.js
@@ -2,3 +2,31 @@ import "./styles.css";
 import { home } from "./home.js";
 import { menu } from "./menu.js";
 import { about } from "./about.js";
+
+const nav = document.querySelector("nav");
+const content = document.querySelector("#content");
+
+nav.addEventListener("click", navClick);
+
+content.appendChild(home());
+
+function navClick(event) {
+  content.removeChild(content.firstChild);
+
+  switch (event.target.id) {
+    case "home": {
+      content.appendChild(home());
+      break;
+    }
+    case "menu": {
+      content.appendChild(menu());
+      break;
+    }
+    case "about": {
+      content.appendChild(about());
+      break;
+    }
+    default:
+      break;
+  }
+}
